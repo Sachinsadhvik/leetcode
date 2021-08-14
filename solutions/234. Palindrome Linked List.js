c25058cc3757a142a26c9215596fe4d0cc4ebdf7
Sachinsadhvik <41516235+Sachinsadhvik@users.noreply.github.com>
2021-08-14 21:29:02 +0530

    164 ms

--- solutions/234. Palindrome Linked List.js
+++ solutions/234. Palindrome Linked List.js
@@ -9,13 +9,36 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
+// var isPalindrome = function(head) {
     
-      var current=head;
-    var x=[]
-    while(current){
-      x.unshift(current.val)
-        current=current.next;
-    }
- return x.join("")==x.reverse().join("");
-};
+//       var current=head;
+//     var arr=[]
+//     while(current){
+//       arr.unshift(current.val)
+//         current=current.next;
+//     }
+//  return arr.every((e, i) => e === arr[arr.length - i - 1])
+// };
+var isPalindrome = function(head) {
+ let p = s = f = head,
+        rev = null
+    // move s to mid
+    while(f && f.next) {
+        f = f.next.next
+        s = s.next
+    }
+    // reverse 2nd half
+    while(s){
+        let t = s.next
+        s.next = rev
+        rev = s
+        s = t
+    }
+    // compare 1st and 2nd half
+    while (rev && p) {
+        if (p.val !== rev.val) return false
+        p = p.next
+        rev = rev.next
+    }
+    return true;
+}  
