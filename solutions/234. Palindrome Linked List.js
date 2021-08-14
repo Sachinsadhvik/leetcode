4e24dfd18c00794b2813d45ef14031b040c1e5a3
Sachinsadhvik <41516235+Sachinsadhvik@users.noreply.github.com>
2021-08-14 21:11:57 +0530

    5144 ms

---
+++ solutions/234. Palindrome Linked List.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    
+      var current=head;
+    var x=[]
+    while(current){
+      x.unshift(current.val)
+        current=current.next;
+    }
+ return x.join("")==x.reverse().join("");
+};
