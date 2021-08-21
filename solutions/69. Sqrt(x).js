0de29eff5e7eb73d54e53a0660583ebd5b1a2841
Sachinsadhvik <41516235+Sachinsadhvik@users.noreply.github.com>
2021-08-21 12:04:31 +0530

    80 ms

---
+++ solutions/69. Sqrt(x).js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+ 
+    let low=0, high=x;
+    while(low<high){
+        const mid= parseInt((low+high)/2);
+        if(mid * mid === x){
+            return mid;
+        }
+        if(x<mid * mid ){
+           high= mid-1
+       } 
+        else {
+            low= mid+1;
+        }
+        
+    }
+    
+    return x < high * high ? high - 1 : high; 
+}
+   // for(let i=0;i<=x;i++){
+   //     if(i*i==x){
+   //         return i;
+   //     }
+   //     else if(i*i>x){return i-1}
+   // } }
