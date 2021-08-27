40820a1da605cdbaabc5e77ffdb127bf9f765830
Sachinsadhvik <41516235+Sachinsadhvik@users.noreply.github.com>
2021-08-27 12:07:58 +0530

    184 ms

--- solutions/148. Sort List.js
+++ solutions/148. Sort List.js
@@ -1,31 +1,42 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+//      * temparary node.
+//      */
+//     while(left !== null || right !== null) {
+//         if (left === null) {
+//             pointer.next = right;
+//             right = right.next;
+//         } else if (right === null) {
+//             pointer.next = left;
+//             left = left.next;
+//         } else {
+//             if (left.val < right.val) {
+//                 pointer.next = left;
+//                 left = left.next;
+//             } else {
+//                 pointer.next = right;
+//                 right = right.next;
+//             }
+//         }
+//         pointer = pointer.next;
+//     }
+​
+//     return root.next;
+// }
 ​
 var sortList = function(head) {
+    current= head;
+   var x=[]
+    while(current){
+        x.push(current.val)
+       current=current.next;  
+    }
+    var middle=head;
+   x.sort((a,b)=>a-b)
+    let i=0;
+    while(middle){
+        middle.val=x[i];
+        i++
+         middle=middle.next;
+    }
     
-    
-    
-    
-}
-​
-// var sortList = function(head) {
-//     current= head;
-//    var x=[]
-//     while(current){
-//         x.push(current.val)
-//        current=current.next;  
-//     }
-//     var middle=head;
-//    x.sort((a,b)=>a-b)
-//      for(let i=0;i<x.length;i++){
-//         middle.val=x[i];
-//          middle=middle.next;
-//     }
-    
-//    return head ;
-// };
+   return head ;
+};
