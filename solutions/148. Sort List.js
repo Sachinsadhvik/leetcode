4085b38733bf22c1e379aecc28e0cb66ce5a4030
Sachinsadhvik <41516235+Sachinsadhvik@users.noreply.github.com>
2021-08-27 11:03:23 +0530

    233 ms

---
+++ solutions/148. Sort List.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+​
+var sortList = function(head) {
+    
+    
+    
+    
+}
+​
+// var sortList = function(head) {
+//     current= head;
+//    var x=[]
+//     while(current){
+//         x.push(current.val)
+//        current=current.next;  
+//     }
+//     var middle=head;
+//    x.sort((a,b)=>a-b)
+//      for(let i=0;i<x.length;i++){
+//         middle.val=x[i];
+//          middle=middle.next;
+//     }
+    
+//    return head ;
+// };
