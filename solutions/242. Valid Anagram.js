54075627484209def10ee06f823245251d73217b
Sachinsadhvik <41516235+Sachinsadhvik@users.noreply.github.com>
2021-08-29 11:31:40 +0530

    96 ms

---
+++ solutions/242. Valid Anagram.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+// var isAnagram = function(s, t) {
+//     if(s.length!=t.length){
+//         return false;
+//     }
+//     else {
+//         return s.split("").sort().join("")==t.split("").sort().join("")
+//     }
+// };
+​
+​
+var isAnagram = function(s, t) {
+  let words={};
+    if(s.length!=t.length) return false;
+    for( i in s){ 
+      words[s[i]]=  words[s[i]]+1 || 1;
+      
+    }
+    for( c in t) {
+      if(!words[t[c]]) return false;
+      words[t[c]]--
+      
+      
+    }
+    
+return true
+}
