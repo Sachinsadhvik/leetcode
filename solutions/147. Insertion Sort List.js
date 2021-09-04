9d167b4f349e42fa86097783506524f92e82a57c
Sachinsadhvik <41516235+Sachinsadhvik@users.noreply.github.com>
2021-09-04 12:42:11 +0530

    138 ms

---
+++ solutions/147. Insertion Sort List.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function insertionSortList(head) {
+    var before = { val: -Number.MAX_VALUE, next: null };
+    
+    while (head) {
+        var prev = before;
+        
+        // find prev
+        while (prev.next && prev.next.val < head.val) {
+            prev = prev.next;
+        }
+        
+        var next = head.next;
+        head.next = prev.next;
+        prev.next = head;
+        head = next;
+    }
+    
+    return before.next;
+}
